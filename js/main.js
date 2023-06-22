cdd57b7e77835be1fd4021887056810d662944c3
abluehope <100765576+abluehope@users.noreply.github.com>
2023-06-22 17:58:52 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,6 +8,30 @@ $(function () {
     }
   });
 
+  $(".main_slide").on("init afterChange", function (e, s, c) {
+    const current = $(".main_slide .slick-current");
+    current.addClass("on").siblings().removeClass("on");
+
+    $(".main_visual .slide_num span").text(c ? c + 1 : 1);
+    $(".main_visual .slide_num strong").text(s.slideCount);
+  });
+
+  $(".main_slide").slick({
+    pauseOnHover: false,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 1000,
+  });
+
+  $(".main_visual .arrows .left").on("click", function () {
+    $(".main_slide").slick("slickPrev");
+  });
+  $(".main_visual .arrows .right").on("click", function () {
+    $(".main_slide").slick("slickNext");
+  });
+
   $(".freature_tab_link li").on("click", function (event) {
     event.preventDefault();
 
@@ -20,5 +44,14 @@ $(function () {
       .addClass("on")
       .siblings()
       .removeClass("on");
+  });
+
+  $(".to_top").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+  });
+
+  $(window).on("scroll", function () {
+    let sct = $(window).scrollTop();
+    sct > 50 ? $(".to_top").addClass("on") : $(".to_top").removeClass("on");
   });
 });
